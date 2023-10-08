c861890876c8eeeed2dffb6d6473e92bb78c6041
raghuprasadks <prasadraghuks@gmail.com>
2023-10-08 16:13:22 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server')
+const cors = require('cors');
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+server.use(cors());
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 8080;  
+server.listen(port, () => {
+  console.log(`JSON Server is running on ${port}`)
+})
